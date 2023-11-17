332008335f89fbdc9bcd6ca188e0a5eb2b1b8e1a
danacmsantiago <136887057+danacmsantiago@users.noreply.github.com>
2023-11-17 15:36:05 +0000

    Deploying to gh-pages from @ danacmsantiago/goit-js-hw-09@721517b19a80e14b6e159cab6c288acd3d02e2fd 🚀

--- 01-color-switcher.99205754.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,colorChange=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(colorChange)}));
-//# sourceMappingURL=01-color-switcher.99205754.js.map
