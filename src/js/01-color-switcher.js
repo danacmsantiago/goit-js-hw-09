79bc32d9d9b8afe7c4a055534c446d11c9d4313c
danacmsantiago <danacmsantiago@gmail.com>
2023-11-17 23:29:59 +0800

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let intervalId;
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', function () {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  colorChange = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', function () {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(colorChange);
+});
