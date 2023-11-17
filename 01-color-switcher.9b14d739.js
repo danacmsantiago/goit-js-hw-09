780dbf64368edc392d99a3c48498194c6f14d226
danacmsantiago <136887057+danacmsantiago@users.noreply.github.com>
2023-11-17 15:30:23 +0000

    Deploying to gh-pages from @ danacmsantiago/goit-js-hw-09@79bc32d9d9b8afe7c4a055534c446d11c9d4313c 🚀

---
+++ 01-color-switcher.9b14d739.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,colorChange=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(colorChange)}))}();
+//# sourceMappingURL=01-color-switcher.9b14d739.js.map
